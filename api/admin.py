5cea95c2443290d68bf29a33d8c092680d772442
KarimovProject <abdullatifkarimov772@gmail.com>
2022-12-01 14:33:25 +0500

    admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import User, Quizes, Subject
+
+# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Quizes)
+admin.site.register(Subject)
