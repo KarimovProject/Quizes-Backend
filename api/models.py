6cbc34be3f433ec85d98beb68c2cbd384a1266de
KarimovProject <abdullatifkarimov772@gmail.com>
2022-12-01 14:33:46 +0500

    models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subject(models.Model):
+    name =models.TextField(max_length=30)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Quizes(models.Model):
+    True_ans = models.TextField(max_length=250)
+    False_ans1 = models.TextField(max_length=250)
+    False_ans1 = models.TextField(max_length=250)
+    False_ans1 = models.TextField(max_length=250)
+    
+    def __str__(self) -> str:
+        return self.True_ans
+    
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    
+    def __str__(self) -> str:
+        return self.username
