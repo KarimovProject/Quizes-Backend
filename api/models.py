788baa2d9ef6be3e2d363eeb1b210e1ee8a00d19
KarimovProject <abdullatifkarimov772@gmail.com>
2022-12-10 15:42:16 +0500

    models update

--- api/models.py
+++ api/models.py
@@ -24,3 +24,26 @@ class Option(models.Model):
 
     def __str__(self):
         return self.title
+
+class User(models.Model):
+    first_name = models.TextField(max_length=100)
+    last_name = models.TextField(max_length=100)
+    password  = models.CharField(max_length=80)
+    email = models.EmailField(max_length=100)
+    def __str__(self) -> str:
+        return self.first_name
+
+class Detail(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz_title = models.CharField(max_length=255)
+    result_detail =models.CharField(max_length=255)
+    def __str__(self) -> str:
+        return self.quiz_title
+    
+class Result_detail(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    detail = models.ForeignKey(Detail, on_delete=models.CASCADE)
+    quation_name = models.CharField(max_length=255)
+    is_solved = models.BooleanField(default=False)
+    def __str__(self) -> str:
+        return self.quation_name
