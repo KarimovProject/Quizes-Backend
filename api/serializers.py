82115015246ead75c5ba490cfa6a7c09e56ab60f
KarimovProject <abdullatifkarimov772@gmail.com>
2022-12-10 15:42:39 +0500

    serializer update

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from .models import Quiz, Question, Option
+from .models import Quiz, Question, Option, User, Detail, Result_detail
 from rest_framework import serializers
 
 class QuizSerializer(serializers.ModelSerializer):
@@ -20,3 +20,22 @@ class OptionSerializer(serializers.ModelSerializer):
         model = Option
         # fields = '__all__'
         exclude = ('is_correct','question')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class DetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+        model = Detail
+        fields = '__all__'
+        
+class ResultDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+        detail = serializers.PrimaryKeyRelatedField(queryset=Detail.objects.all())
+        model = Result_detail
+        fields = '__all__'
+        
